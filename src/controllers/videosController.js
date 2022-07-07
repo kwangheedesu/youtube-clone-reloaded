aa83cf452a3f409507b5eb7b1c6c8e79359a13cf
kwangheedesu <kwang5204@naver.com>
2022-07-08 06:12:34 +0900

    practice
    practice

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -45,3 +45,20 @@ export const postEdit = (req,res) => {
    return res.redirect(`/videos/${id}`);
 };
 
+export const getUpload = (req,res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
+
+export const postUpload = (req,res) => {
+    const { title } = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comment: 0,
+        createdAt: "just now",
+        views: 0,
+        id: videos.length + 1,
+    }
+    videos.push(newVideo);
+    return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {watch, upload, deleteVideo, getEdit, postEdit,} from "../controllers/videosController";
+import {watch, getUpload, getEdit, postEdit, postUpload,} from "../controllers/videosController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 
 
